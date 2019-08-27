dbef934d94f580cdc9f4494bbafd70475a3a6f98
SRGTxTwinkie <54576833+SRGTxTwinkie@users.noreply.github.com>
2019-08-27 05:44:11 -0400

    Add files via upload

---
+++ Sarcasm-Inator.py
@@ -0,0 +1,15 @@
+import random, time
+class values:
+    z = []
+    x = input('Enter the string: ')
+    y = random.randint(1,2)
+    b = []
+def sarcasm():
+    values.x
+    print(values.x)
+    
+    
+
+sarcasm()
+print(values.b)
+time.sleep(4)
